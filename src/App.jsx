35d22c40e44745e58d9f243651e2e32a0eda7241
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-23 00:04:04 +0100

    style: import css in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-
+import './App.css';
 
 function App() {
   return (
