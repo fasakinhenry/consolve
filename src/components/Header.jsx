521b9320497a4bdb29d10879164c3022c3def0ac
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-23 00:35:44 +0100

    feat: update Header component
    Add improved styling
    use logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => {
-    return (
-        <header className="flex justify-between items-center py-4 px-6">
-            <div className="text-xl font-bold text-[#fdca3a]">tuliban</div>
-            <nav>
-                <a href="#" className="text-gray-300 mx-2 hover:text-white">Developers</a>
-                <a href="#" className="text-gray-300 mx-2 hover:text-white">Customers</a>
-                <a href="#" className="bg-[#fdca3a] text-black px-4 py-2 rounded-full hover:bg-yellow-500">Login</a>
-            </nav>
-        </header>
-    );
+  return (
+    <header className='py-4'>
+      <div className='container'>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center'>
+            <img src='/tuliban-logo.png' alt='Tuliban' className='h-8 mr-2' />
+            <span className='text-xl font-bold text-[#fdca3a]'>tuliban</span>
+          </div>
+          <nav className='flex items-center'>
+            <a href='#' className='text-gray-300 mx-2 hover:text-white'>
+              Developers ▼
+            </a>
+            <a href='#' className='text-gray-300 mx-2 hover:text-white'>
+              Customers
+            </a>
+            <a
+              href='#'
+              className='bg-[#fdca3a] text-black px-4 py-2 rounded-full hover:bg-yellow-500 flex items-center'
+            >
+              <FaGithub className='mr-2' />
+              Login
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
