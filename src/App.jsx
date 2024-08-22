1f7214eb23d245c32a67495231d1b5eead0a6456
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-23 00:03:35 +0100

    refactor: refactor app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
+
 function App() {
   return (
     <Router>
