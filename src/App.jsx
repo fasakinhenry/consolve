d4c09bded6d1521a79ade67f2ef2968c7c793e27
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-27 00:50:22 +0100

    feat: update App with Dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import './index.css';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Other routes */}
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
