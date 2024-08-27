4d87a95a2630df5a6d4dac9b7c586da9f9243345
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-27 01:03:48 +0100

    feat: create the dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+import DashboardContent from '../components/DashboardContent';
+
+const Dashboard = () => {
+  return (
+    <MainLayout>
+      <DashboardContent />
+    </MainLayout>
+  );
+};
+
+export default Dashboard;
