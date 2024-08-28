a92ae572b0b566067d234f43a1cff7a5a5b437d7
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-28 16:19:06 +0100

    feat: add all the routes in the main app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Providers from './pages/Providers';
+import Inbox from './pages/Inbox';
+import Appointments from './pages/Appointments';
+import Community from './pages/Community';
+import Services from './pages/Services';
+import Wallet from './pages/Wallet';
+import Logistics from './pages/Logistics';
+import Bounties from './pages/Bounties';
+import Events from './pages/Events';
+import Tools from './pages/Tools';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import './index.css';
@@ -27,6 +36,78 @@ function App() {
           element={
             <MainLayout>
               <Providers />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/inbox'
+          element={
+            <MainLayout>
+              <Inbox />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/appointments'
+          element={
+            <MainLayout>
+              <Appointments />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/community'
+          element={
+            <MainLayout>
+              <Community />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/services'
+          element={
+            <MainLayout>
+              <Services />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/wallet'
+          element={
+            <MainLayout>
+              <Wallet />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/logistics'
+          element={
+            <MainLayout>
+              <Logistics />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/bounties'
+          element={
+            <MainLayout>
+              <Bounties />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/events'
+          element={
+            <MainLayout>
+              <Events />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/tools'
+          element={
+            <MainLayout>
+              <Tools />
             </MainLayout>
           }
         />
