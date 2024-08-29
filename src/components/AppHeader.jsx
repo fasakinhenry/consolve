e6ff4aa57e6094217a2f47ccffda4b885d43f892
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-29 05:57:19 +0100

    style: update app name and logo in app header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -7,8 +7,8 @@ const AppHeader = ({ toggleSidebar }) => {
     <header className="bg-gray-800 lg:hidden">
       <div className="px-4 py-3 flex items-center justify-between">
         <div className="flex items-center">
-          <img src="/path-to-your-logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-          <span className="text-xl font-semibold text-white">Tublian</span>
+          <img src="/consolve-logo.png" alt="Logo" className="h-8 w-8 mr-2" />
+          <span className="text-xl font-semibold text-white">Consolve</span>
         </div>
         <button onClick={toggleSidebar} className="text-white">
           <HambergerMenu size="24" />
