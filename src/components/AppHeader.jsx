9c1a76597a46223c439c35e057d5d18458d26fdd
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-29 05:58:00 +0100

    style: change color of app header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,7 +4,7 @@ import { HambergerMenu } from 'iconsax-react';
 
 const AppHeader = ({ toggleSidebar }) => {
   return (
-    <header className="bg-gray-800 lg:hidden">
+    <header className="bg-[#121212] lg:hidden">
       <div className="px-4 py-3 flex items-center justify-between">
         <div className="flex items-center">
           <img src="/consolve-logo.png" alt="Logo" className="h-8 w-8 mr-2" />
